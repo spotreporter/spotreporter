83bcf2ebe849e33fc5f12a2717179852dafa1ee8
Lukas Hass <lukas@slucky.de>
2021-02-04 11:57:09 +0100

    chore: add vue linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,17 @@
 /* eslint-env node */
 module.exports = {
   root: true,
-  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+  },
+  parser: 'vue-eslint-parser',
   parserOptions: {
+    parser: '@typescript-eslint/parser',
     project: ['./tsconfig.eslint.json'],
     sourceType: 'module',
     ecmaVersion: 2018,
     tsconfigRootDir: __dirname,
+    extraFileExtensions: ['.vue'],
   },
   plugins: ['@typescript-eslint'],
   extends: [
@@ -16,6 +21,11 @@ module.exports = {
 
     'prettier/@typescript-eslint',
     'plugin:prettier/recommended',
+    'plugin:vue/vue3-recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint',
+    'plugin:vue-scoped-css/recommended',
   ],
   rules: {
     // make typescript eslint rules even more strict
@@ -23,21 +33,35 @@ module.exports = {
     '@typescript-eslint/no-unused-vars': 'error',
     '@typescript-eslint/explicit-module-boundary-types': 'error',
     '@typescript-eslint/no-non-null-assertion': 'error',
+    'vue/attribute-hyphenation': 'error',
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+        },
+      },
+    ],
+    'vue/component-name-in-template-casing': 'error',
+    'vue/no-static-inline-styles': 'error',
+    'vue/v-on-function-call': 'error',
+    'vue/no-useless-v-bind': 'error',
+    'vue/no-useless-mustaches': 'error',
+    'vue/no-useless-concat': 'error',
+    'vue/no-boolean-default': 'error',
+    // css rules
+    'vue-scoped-css/no-unused-selector': 'error',
+    'vue-scoped-css/no-parsing-error': 'error',
+    'vue-scoped-css/require-scoped': 'error',
   },
   overrides: [
-    {
-      files: ['**/*.test.(t|j)s', '**/*.spec.(t|j)s'],
-      env: {
-        jest: true, // now **/*.test.js files' env has both es6 *and* jest
-      },
-      extends: ['plugin:jest/all'],
-    },
     {
       files: ['**/*.js'],
       rules: {
         '@typescript-eslint/no-unsafe-assignment': 'off',
         '@typescript-eslint/no-unsafe-member-access': 'off',
         '@typescript-eslint/no-unsafe-call': 'off',
+        '@typescript-eslint/restrict-plus-operands': 'off',
         '@typescript-eslint/no-var-requires': 'off',
       },
     },
